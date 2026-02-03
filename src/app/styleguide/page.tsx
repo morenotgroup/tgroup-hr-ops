b1322c53f02e1140ee0941358057e31892a12693
morenotgroup <gc@agenciataj.com>
2026-02-02 23:29:13 -0300

    Fix parallax transform typing

--- src/app/styleguide/page.tsx
+++ src/app/styleguide/page.tsx
@@ -53,7 +53,10 @@ export default function StyleguidePage() {
   const smoothY = useSpring(mouseY, { stiffness: 70, damping: 18 });
   const { scrollYProgress } = useScroll();
   const scrollShift = useTransform(scrollYProgress, [0, 1], [0, 48]);
-  const parallaxY = useTransform([smoothY, scrollShift], ([y, scroll]) => y + scroll);
+  const parallaxY = useTransform(
+    [smoothY, scrollShift] as const,
+    ([y, scroll]: [number, number]) => y + scroll
+  );
 
   useEffect(() => {
     if (reduceMotion) return;
